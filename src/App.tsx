943afeb8006e836a7176052be37c58fee7d9b91f
Eko Putra Wahyuddin <eko.putrawahyu56@gmail.com>
2023-09-15 17:14:30 +0700

    feat: initial change

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import image from "./images/bg1.webp";
+import React from "react";
 
 function App() {
+  const [isScale, setIsScale] = React.useState(false);
+
+  const handleScroll = () => {
+    setIsScale((prev) => window.scrollY > 50);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main relative">
+      <div
+        style={{
+          backgroundImage: `url(${image})`,
+          height: "100vh",
+          backgroundPosition: "0px -100px",
+        }}
+        className={`w-full fixed bg-first-position transition-all duration-2s ${
+          isScale ? "scale-effect" : ""
+        }`}
+      >
+        <div className="flex w-100 h-full items-center justify-center bg-black bg-opacity-50">
+          <h1 className="main-title text-center font-bold text-white w-3/5 transition-all duration-2s">
+            Sampai kapan Kawasan Timur Indonesia berada dalam wilayah rawan
+            pangan?
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
